2b7e73a2624a5be38f1443980e1247626332d4b4
Oleg Subachev <git@solsw.ru>
2025-09-08 16:15:51 +0500

    math/rand package replaced with math/rand/v2

--- cryptorand.go
+++ cryptorand.go
@@ -5,26 +5,23 @@ import (
 	cryptorand "crypto/rand"
 	"encoding/binary"
 	"io"
-	mathrand "math/rand"
+	mathrandv2 "math/rand/v2"
 )
 
-// NewCryptoRand returns [math/rand.Rand] instance backed by [crypto/rand].
-func NewCryptoRand() *mathrand.Rand {
-	crs := cryptoRandSource{bufio.NewReader(cryptorand.Reader)}
-	return mathrand.New(crs)
+// NewCryptoRand returns [math/rand/v2/Rand] instance backed by [crypto/rand].
+func NewCryptoRand() *mathrandv2.Rand {
+	crs := cryptoRandSource{byteReader: bufio.NewReader(cryptorand.Reader)}
+	return mathrandv2.New(crs)
 }
 
-// cryptoRandSource implements the [math/rand.Source] interface.
+// cryptoRandSource implements the [math/rand/v2/Source] interface.
 type cryptoRandSource struct {
 	// inspired by github.com/andrew-d/csmrand
 	byteReader io.ByteReader
 }
 
-// Int63 implements the [math/rand.Source.Int63] method.
-func (s cryptoRandSource) Int63() int64 {
+// Uint64 implements the [math/rand/v2/Source.Uint64] method.
+func (s cryptoRandSource) Uint64() uint64 {
 	u64, _ := binary.ReadUvarint(s.byteReader)
-	return int64(u64 >> 1)
+	return u64
 }
-
-// Seed implements the [math/rand.Source.Seed] method.
-func (cryptoRandSource) Seed(int64) {}
